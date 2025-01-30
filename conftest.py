7af087d9b0d31d308da5713cdcca0e114492bcea
Sergey B Kirpichev <skirpichev@gmail.com>
2025-01-30 05:30:22 +0300

    Add python-gmp as optional backend (gmpy2 alternative)
    Closes #895

--- conftest.py
+++ conftest.py
@@ -15,6 +15,10 @@ def pytest_report_header(config):
     print("mpmath mp class: %s" % repr(mpmath.mp))
     print("mpmath version: %s" % mpmath.__version__)
     print("Python version: %s" % sys.version)
+    if mpmath.libmp.backend.BACKEND == 'gmp':
+        import gmp
+        print(gmp.gmp_info)
+        print("python-gmp version: %s" % gmp.__version__)
 
 
 def pytest_configure(config):
